77ba17699aada144aeae311601ba740d13cb5e06
Ankit Kumar Pandit <ankitp@geekyants.com>
2021-03-28 21:09:40 +0530

    Heroku deploy

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const cors_1 = __importDefault(require("cors"));
 const http_1 = __importDefault(require("http"));
 const express_1 = __importDefault(require("express"));
 const socket_io_1 = require("socket.io");
@@ -12,7 +11,6 @@ const socket_1 = __importDefault(require("./src/socket"));
 const port = process.env.PORT || 8021;
 const app = express_1.default();
 const httpServer = http_1.default.createServer(app);
-app.use(cors_1.default);
 app.use(router_1.default);
 httpServer.listen(port, () => {
     console.log(`Server started on port ${port}`);
